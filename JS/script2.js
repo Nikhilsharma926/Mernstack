830ef4b01d860a9e510b59edc9304ff4c9c35a49
Nikhilsharam962 <niksharma787683@gmail.com>
2025-09-20 22:30:46 +0530

    ask the user for a number

---
+++ JS/script2.js
@@ -0,0 +1,7 @@
+let val = prompt("enter a number =");
+for(i=0; i<=val; i++){
+    if(i%2===0){
+        console.log(`${i} is a even number`)
+    }else{
+    console.log(`${i} is a odd number`)}
+}
