ba6f497398b726b1f171aeb6aeb375ecb62d8a97
Nikhilsharam962 <niksharma787683@gmail.com>
2025-09-24 15:00:25 +0530

    js nooB

--- JS/script2.js
+++ JS/script2.js
@@ -1,7 +1,66 @@
-let val = prompt("enter a number =");
-for(i=0; i<=val; i++){
-    if(i%2===0){
-        console.log(`${i} is a even number`)
-    }else{
-    console.log(`${i} is a odd number`)}
+// let val = prompt("enter a number =");
+// for(i=0; i<=val; i++){
+//     if(i%2===0){
+//         console.log(`${i} is a even number`)
+//     }else{
+//     console.log(`${i} is a odd number`)}
+// }
+
+
+// function //
+
+sasa();  // hoisting is possible
+
+function sasa(){
+    console.log("a")  // function decleartion & statements-----------------------------------
+
 }
+
+// function expression ----------------------------------------------------------------------
+
+aa(); // hoisting is not possible
+
+let aa = function(){
+    console.log("a")
+
+}
+
+//arrow function------------------------------------------------------------------- 
+
+let ds = (a,b)=> {    // a b is parameter
+    console.log(a+b)
+}
+
+ds(1,1);  // 1 1 is arguments
+
+
+// parameter & argument------------------------------------------------------
+  // with statement & decleration
+function sum(a,b){
+    console.log(a+b)
+
+}
+sum(1,1);   // same as arrow fxn
+
+// with fxn expression ---------------------------------------------------------
+
+let asdf = function(v1){ // v1 is parameter
+    console.log(`${v1} i am learing javascript `)
+
+}
+asdf("today") //  today is arguments
+
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
